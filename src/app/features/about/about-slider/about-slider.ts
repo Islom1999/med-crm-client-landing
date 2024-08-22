c570b5cba71f4dea4abd123f45841adf8f2d3739
Asror Shodiyev <asrorshodiyev@Asror-2.local>
2024-08-22 09:35:06 +0300

    about slider tuzatildi

--- src/app/features/about/about-slider/about-slider.ts
+++ src/app/features/about/about-slider/about-slider.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SwiperOptions } from 'swiper/types';
+
+
 
 @Component({
 	selector: 'app-about-slider',
@@ -12,24 +13,5 @@ import { SwiperOptions } from 'swiper/types';
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AboutSlider {
-
-	config: SwiperOptions = {
-		slidesPerView: 3, // Show 3 items
-		centeredSlides: true, // Center the active slide
-		spaceBetween: -100, // Adjust this value to control how much of the items should be off-screen
-		loop: true, // Loop through slides
-		navigation: true, // Navigation arrows
-		breakpoints: {
-			640: {
-				slidesPerView: 3, // Adjust for smaller screens
-			},
-			768: {
-				slidesPerView: 3, // Adjust for medium screens
-			},
-			1024: {
-				slidesPerView: 3, // Default setting for larger screens
-			},
-		}
-	};
-
+	
 }
