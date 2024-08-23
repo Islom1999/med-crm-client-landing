c8b129d777d336010be166c4e9dd2a8550900243
Asror Shodiyev <asrorshodiyev@Asror-2.local>
2024-08-24 01:08:36 +0300

    navbar desktop xatolar tuzatildi

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -12,10 +13,12 @@ import { FooterComponent } from './components/footer/footer.component';
 	],
 	imports: [
 		CommonModule,
+		RouterModule
 	],
 	exports: [
 		NavbarComponent,
 		FooterComponent,
+		RouterModule
 	]
 })
 export class SharedModule { }
