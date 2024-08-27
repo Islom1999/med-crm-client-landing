93f693ae51f63857a2f24010927424fe74396a4e
Asror Shodiyev <asrorshodiyev@Asror-2.local>
2024-08-28 02:46:04 +0300

    accordionlar qo'shildi

--- src/app/features/hospital/hospital.component.ts
+++ src/app/features/hospital/hospital.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-hospital',
-  templateUrl: './hospital.component.html',
-  styleUrl: './hospital.component.scss'
+	selector: 'app-hospital',
+	templateUrl: './hospital.component.html',
+	styleUrl: './hospital.component.scss'
 })
 export class HospitalComponent {
+	openIndex: number | null = null;
+
+	accordionToggle(index: number) {
+		if (this.openIndex === index) {
+			this.openIndex = null;
+		} else {
+			this.openIndex = index;
+		}
+	}
 
 }
