1e523e109634f82e1ca9f09bfcb41d9c50b69c9f
Asror Shodiyev <asrorshodiyev@Asror-2.local>
2024-08-29 09:24:07 +0300

    contact qo'shildi

--- src/app/features/contacts/contacts.module.ts
+++ src/app/features/contacts/contacts.module.ts
@@ -2,23 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsComponent } from './contacts.component';
 import { Routes, RouterModule } from '@angular/router';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ContactsComponent
-  }
+	{
+		path: '',
+		component: ContactsComponent
+	}
 ];
 
-
 @NgModule({
-  declarations: [
-    ContactsComponent
-  ],
-  imports: [
-    CommonModule,
-
-    RouterModule.forChild(routes)
-  ]
+	declarations: [
+		ContactsComponent
+	],
+	imports: [
+		CommonModule,
+		RouterModule.forChild(routes),
+		NgxMaskDirective,  // Mask Direktivasini qo'shish
+		NgxMaskPipe
+	],
+	providers: [
+		provideNgxMask()   // NgxMask provayderini qo'shish
+	]
 })
 export class ContactsModule { }
