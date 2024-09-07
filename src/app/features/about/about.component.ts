95911fd31d60e7001a05a22a73f83c75c238d828
Karimov Islom <islomkarimovalisherovich@gmail.com>
2024-09-07 23:22:30 +0500

    PrimeNG and PrimeFlex added this project

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -6,6 +6,8 @@ import * as AOS from 'aos'
 	styleUrl: './about.component.scss'
 })
 export class AboutComponent implements OnInit {
+	$header:{image?: string | null, title: string, descr:string} = {image: null, title: "Bizning GLOBAL HEALTHCARE shifoxonamiz", descr:"Abat Steklo - shisha va shisha buyumlar ishlab chiqarishga ixtisoslashgan kompaniya. 2013-yilda tashkil etilgan kompaniya bir necha yillardan buyon o‘z mijozlariga yuqori sifatli shisha yechimlarini taqdim etib kelmoqda. Ularning xizmatlari oyna, eshik, oyna va dekorativ oyna buyumlari kabi turli xil oyna mahsulotlarini ishlab chiqarishni o'z ichiga oladi. Abat Steklo sifat va mijozlar ehtiyojini qondirishga sodiqligi bilan mashhur bo'lib, ularni shisha bilan bog'liq barcha ehtiyojlar uchun ishonchli tanlovga aylantiradi."}
+
 	ngOnInit() {
 		AOS.init();
 	}
