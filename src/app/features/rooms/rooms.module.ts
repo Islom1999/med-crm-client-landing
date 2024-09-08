1fca08467745159e3d1c23bbf682714b3ffe2134
Asror Shodiyev <asrorshodiyev@Asror-2.local>
2024-09-08 16:25:37 +0300

    rooms sahifa responsive bo'ldi

--- src/app/features/rooms/rooms.module.ts
+++ src/app/features/rooms/rooms.module.ts
@@ -1,26 +1,27 @@
 import { NgModule } from '@angular/core';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { CommonModule } from '@angular/common';
 import { RoomsComponent } from './rooms.component';
 import { Routes, RouterModule } from '@angular/router';
 import { PageType } from '../../../enumerations';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: RoomsComponent,
-    data: {page_type: PageType.list}
-  }
+	{
+		path: '',
+		component: RoomsComponent,
+		data: { page_type: PageType.list }
+	}
 ];
 
 
 @NgModule({
-  declarations: [
-    RoomsComponent
-  ],
-  imports: [
-    CommonModule,
-
-    RouterModule.forChild(routes)
-  ]
+	declarations: [
+		RoomsComponent
+	],
+	imports: [
+		CommonModule,
+		ProgressSpinnerModule,
+		RouterModule.forChild(routes)
+	]
 })
 export class RoomsModule { }
